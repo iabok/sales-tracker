2978f04b085176e8372af5325e6b589bd9bd968a
Abok Isaac <abokisaac@gmail.com>
2017-07-18 22:57:11 +0300

    Improved the form rendering

--- web/sales_app/apps/stations/views.py
+++ web/sales_app/apps/stations/views.py
@@ -1,7 +1,25 @@
+'''
+ Stations views
+'''
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from stations.models import Station
+# from stations.forms import StationForm
 
+
+# class StationView(FormView):
+#     '''
+#      Creates and saves our form
+#     '''
+#     template_name = '../templates/station_add_form.html'
+#     form_class = StationForm
+#     success_url = '/thanks/'
+#
+#     def form_valid(self, form):
+#         '''
+#          Saves our form
+#         '''
+#         return super(StationView, self).form_valid(form)
 
 class StationCreate(CreateView):
     '''
@@ -11,12 +29,12 @@ class StationCreate(CreateView):
     template_name = '../templates/station_add_form.html'
     fields = ['name', 'location']
 
-    def form_valid(self, form):
-        '''
-         Save our form
-        '''
-        # form.instance.created_by = self.request.user
-        return super(StationCreate, self).form_valid(form)
+    # def form_valid(self, form):
+    #     '''
+    #      Save our form
+    #     '''
+    #     # form.instance.created_by = self.request.user
+    #     return super(StationCreate, self).form_valid(form)
 
 
 class StationUpdate(UpdateView):
